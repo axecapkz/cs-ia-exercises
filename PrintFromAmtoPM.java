87e3fd0da4eca897679c9eaa5ae835c8c405c27a
Azamat <educationonline@me.com>
2021-03-22 04:39:30 +0600

    Update PrintFromAmtoPM.java

--- PrintFromAmtoPM.java
+++ PrintFromAmtoPM.java
@@ -1,7 +1,6 @@
 public class PrintFromAmtoPM {
     public static void main(String[] args) {
-        boolean midnight = false;
-
+        
         for (int i = 0; i < 12; i++) {
             if (i == 0)
                 System.out.println(i + ":00" + " Midnight");
